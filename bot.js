31ffe202d60f4dc90e02c08f0a92829cbf12db44
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-02 20:47:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-
+const prefix = 'r!'
    client.on("message", message => {
  if(message.content.startsWith(prefix + "set")) {
    let rainbow = message.guild.roles.find(`name`, "Rainbow");
